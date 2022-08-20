48725f53c822a86a279d3b39981974ef93329200
Peter Bom Jensen <peterbom86@gmail.com>
2022-08-20 14:24:19 +0200

    added firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { LoadFactions, LoadSubfactions, LoadUnits } from './store/units/unit.actions';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCKu_N7Nyaskx5Q_vooYa_tEkB45t-2U9I",
+  authDomain: "niebelunggame.firebaseapp.com",
+  projectId: "niebelunggame",
+  storageBucket: "niebelunggame.appspot.com",
+  messagingSenderId: "946038869683",
+  appId: "1:946038869683:web:612b2c1dc7230b04722ac6",
+  measurementId: "G-2HMHN57ZGK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,6 +32,9 @@ import { LoadFactions, LoadSubfactions, LoadUnits } from './store/units/unit.act
 export class AppComponent implements OnInit {
 
   constructor(private store: Store) { }
+
+
+
 
   ngOnInit(): void {
     this.store.dispatch(new LoadUnits());
