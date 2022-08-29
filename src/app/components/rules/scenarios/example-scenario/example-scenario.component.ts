89f3f3718327a2f1a4cf5ca87fe7e707d990df50
Peter Bom Jensen <peterbom86@gmail.com>
2022-08-30 00:00:49 +0200

    added terrain and example scenario

--- src/app/components/rules/scenarios/example-scenario/example-scenario.component.ts
+++ src/app/components/rules/scenarios/example-scenario/example-scenario.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-example-scenario',
   templateUrl: './example-scenario.component.html',
-  styleUrls: ['./example-scenario.component.scss']
+  styleUrls: ['./example-scenario.component.scss', './../../rules.component.scss']
 })
 export class ExampleScenarioComponent implements OnInit {
 
